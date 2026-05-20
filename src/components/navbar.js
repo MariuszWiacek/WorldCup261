e70b3bd0d33050eef5f3cd7a9b4e1fdb20b87c09
Mariusz Wiacek <117862308+MariuszWiacek@users.noreply.github.com>
2026-05-20 01:52:25 +0100

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
     position: 'fixed',
     top: 0,
     width: '100%',
-    background: scrollPosition > 0 ? '#a3953ef1' : 'black',
+    background: scrollPosition > 0 ? 'grey' : 'black',
     zIndex: 1000,
     transition: 'background-color 0.3s ease',
   };
